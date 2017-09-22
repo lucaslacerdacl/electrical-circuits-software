a577b2b71b8e27644a30308ef65f99981e6f52fb
Lucas Leandro Costa Lacerda <lucas.lacerdabh@gmail.com>
2017-09-22 16:31:29 -0300

    Improve the 'Hello World' with constants, console in and strings

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+const string helloWorld("Hello World!");
+const char breakLine = '\n';
+
 int main() {
+
+    string title("Please type some string and press enter: ");
+    string i;
     
-    cout << "Hello World!" << endl;
+    cout << helloWorld << breakLine << title << endl;
+
+    cin >> i;
 
     return 0;
 }
